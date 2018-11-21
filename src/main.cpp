592811453fd6cd00b8fadc0ec5ad5f685b002470
Aaron <aaron.winkler98@googlemail.com>
2018-11-21 21:29:09 +0100

    v2.0 - passthrough

--- src/main.cpp
+++ src/main.cpp
@@ -16,19 +16,24 @@ const char* mqtt_server = "192.168.0.108";
 int receiver = 4;   //D2
 
 //receive variables
-const int pir_sensor_1_motion = 13100;
 int prev_signal = 0;
 unsigned long timestamp = 0;
 
+//transmit variables
+//...
+
 /* MQTT subscriptions
-- /all/aaron/light/chain/chain_set
-- /all/aaron/light/chain/chain_setbrightness
+- /all/aaron/bridge/433/433_transmit_command
  */
 
-/* MQTT pushs
-- /all/aaron/light/chain/chain_state
-- /all/aaron/motion_detect/pir1_detected
- */
+ /* MQTT publish
+ - /all/aaron/bridge/433/433_receive_command
+  */
+
+
+  // void declaration
+  void setup_wifi();
+  void reconnect();
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -36,6 +41,96 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 
+
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  String tp = topic;
+  char message[] = "";
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    message[i] = (char)payload[i];
+  }
+
+  int message_int = atoi(message);
+
+
+  Serial.println();
+
+/*
+  if (tp.indexOf("/all/aaron/bridge/ir/ir_transmit_command")>=0) {
+    char send_code[] = "";
+    send_code[0] = ir_remote[0];
+    send_code[1] = ir_remote[1];
+    for (size_t i = 2; i < length+2; i++) {
+      send_code[i] = payload[i];
+    }
+
+    mySwitch.sendTriState(send_code);
+    Serial.print("IR send: ");
+    Serial.println(send_code);
+
+  }
+*/
+  if (tp.indexOf("/all/aaron/bridge/433/433_transmit_command")>=0) {
+    myReceiver.disableReceive();
+    mySwitch.send(message_int, 24);
+    myReceiver.enableReceive(receiver);
+    Serial.print("433 send: ");
+    Serial.println(message_int);
+  }
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("setup begin");
+  mySwitch.enableTransmit(2);
+  myReceiver.enableReceive(receiver);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  //mySwitch.send(23600,24);
+  Serial.println("setup finished");
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+
+
+  if (myReceiver.available()) {
+
+    int signal = myReceiver.getReceivedValue();
+    char signal_c[] = "";
+    itoa(signal, signal_c, 10);
+
+    Serial.println("");
+    Serial.print("message received: ");
+    Serial.println(signal);
+
+    if ((!(signal == prev_signal) || millis() - timestamp > 2000) && (signal > 0)){
+      prev_signal = signal;
+      client.publish("/all/aaron/bridge/433/433_receive_command", signal_c, sizeof(signal_c));
+    }
+    else{
+      Serial.println("received multiple times, wrong format or '23040'");
+    }
+
+  }
+
+  myReceiver.resetAvailable();
+
+}
 
 void setup_wifi() {
 
@@ -60,54 +155,6 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  String message = "";
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (unsigned int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-    message = message + (char)payload[i];
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if (message.indexOf("true") >= 0) {
-    Serial.println("true");
-    mySwitch.send(23600, 24);
-    Serial.println("send: 23600");
-    client.publish("/all/aaron/light/chain/chain_state", "true");
-  }
-  else if (message.indexOf("false") >= 0) {
-    Serial.println("false");
-    mySwitch.send(23500, 24);
-    Serial.println("send: 23500");
-    client.publish("/all/aaron/light/chain/chain_state", "false");
-  }
-  else if (message.toInt() == 40) {
-    Serial.println("got 40 --> set 41");
-    mySwitch.send(23041, 24);
-    Serial.println("send: 23041");
-    client.publish("/all/aaron/light/chain/chain_state", "41");
-  }
-  else if (message.toInt() <= 100 || (message.toInt() >= 0)) {
-    Serial.print("set brightness to: ");
-    Serial.print(message);
-    Serial. println("%");
-    int value = 23000;
-    value = value + message.toInt();
-    Serial.print("send: ");
-    Serial.println(value);
-    mySwitch.send(value, 24);
-    client.publish("/all/aaron/light/chain/chain_state", payload, sizeof(value - 23000));
-  }
-  else {
-    Serial.println("unknown command");
-    client.publish("/all/aaron/light/chain/chain_state", "unknown");
-  }
-
-}
-
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
@@ -119,12 +166,10 @@ void reconnect() {
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("/all/aaron/motionDetector/pirBed/pirBed_detected", "started");
-      client.publish("/all/aaron/light/chain/chain_state", "started");
-      //client.publish("/all/aaron/motion_detector/pir1/pir1_detected", "started");
+      client.publish("/all/aaron/bridge/433/433_transmit_command", "0");
+      client.publish("/all/aaron/bridge/433/433_receive_command", "0");
       // ... and resubscribe
-      client.subscribe("/all/aaron/light/chain/chain_set");
-      client.subscribe("/all/aaron/light/chain/chain_setbrightness");
+      client.subscribe("/all/aaron/bridge/433/433_transmit_command");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -133,53 +178,4 @@ void reconnect() {
       delay(5000);
     }
   }
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("setup begin");
-  mySwitch.enableTransmit(2);
-  myReceiver.enableReceive(receiver);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  Serial.println("setup finished");
-}
-
-void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-
-
-  if (myReceiver.available()) {
-
-
-    int signal = myReceiver.getReceivedValue();
-
-    Serial.println("");
-    Serial.print("message received: ");
-    Serial.println(signal);
-
-    if ((!(signal == prev_signal) || millis() - timestamp > 2000) && (signal > 0) && !(signal == 23040)){  //23040 is a frequently send code (seems to be an error - needs to be catched)
-      prev_signal = signal;
-      switch (signal) {
-        case pir_sensor_1_motion: //Serial.println("PIR Sensor 1 reports movement");
-                                  //client.loop();
-                                  client.publish("/all/aaron/motionDetector/pirBed/pirBed_detected", "true");
-                                  //Serial.println("mqqt published");
-                                  timestamp = millis();
-                                  break;
-      }
-    }
-    else{
-      Serial.println("received multiple times, wrong format or '23040'");
-    }
-
-  }
-
-  myReceiver.resetAvailable();
-
 }
